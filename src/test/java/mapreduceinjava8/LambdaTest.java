87b3ba320a68814efbe3c1c6817e7728076f9ce1
Marek Dudek <marek.dudek@gmail.com>
2015-03-18 22:29:15 +0100

    Reading data from Data project works

--- src/test/java/mapreduceinjava8/LambdaTest.java
+++ src/test/java/mapreduceinjava8/LambdaTest.java
@@ -1,18 +1,25 @@
 package mapreduceinjava8;
 
+import com.google.common.base.Charsets;
+import mapreducedata.FileAccessor;
+import org.apache.commons.io.IOUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class LambdaTest {
 
     @Test
-    public void test() {
+    public void lambdas_should_work() {
 
         // given
         final List<Integer> numbers = asList(1, 2, 3, 4, 5);
@@ -25,5 +32,26 @@ public class LambdaTest {
 
         // then
         assertThat(even, hasSize(2));
+    }
+
+    @Test
+    public void data_from_other_project_should_be_accessible() {
+
+        // given
+        final List<String> lines = FileAccessor.readLinesFromResources("us/counties.csv");
+
+        // then
+        assertThat(lines, hasSize(3142));
+    }
+
+    @Test
+    public void test() throws IOException {
+
+        // when
+        final InputStream stream = getClass().getClassLoader().getResourceAsStream("us/counties.csv");
+        final List<String> lines = IOUtils.readLines(stream);
+
+        // then
+        assertThat(lines, hasSize(3142));
     }
 }
