d3b98c1a3c3a683a7f8ea4d5b2a38279ff24fc00
Marek Dudek <marek.dudek@gmail.com>
2015-03-22 00:34:54 +0100

    Map comparison matcher

--- src/test/java/mapreduceinjava8/utils/HasEqualContents.java
+++ src/test/java/mapreduceinjava8/utils/HasEqualContents.java
@@ -1,29 +1,66 @@
 package mapreduceinjava8.utils;
 
+import com.google.common.collect.Sets;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 
 import java.util.Map;
 
-public class HasEqualContents<KEY, VALUE> extends TypeSafeMatcher<Map<? extends KEY, ? extends VALUE>> {
+public class HasEqualContents<KEY, VALUE> extends TypeSafeMatcher<Map<KEY, VALUE>> {
 
     private final Map<KEY, VALUE> expected;
+
+    private Object difference;
+
+    Sets.SetView<Map.Entry<KEY, VALUE>> missingInExpected;
+    Sets.SetView<Map.Entry<KEY, VALUE>> obsoleteInActual;
 
     public static <KEY, VALUE> HasEqualContents<KEY, VALUE> hasEqualContents(final Map<KEY, VALUE> expected) {
         return new HasEqualContents<>(expected);
     }
 
-    private HasEqualContents(final Map<KEY, VALUE> expected) {
+    HasEqualContents(final Map<KEY, VALUE> expected) {
         this.expected = expected;
     }
 
     @Override
-    protected boolean matchesSafely(final Map<? extends KEY, ? extends VALUE> actual) {
-        return expected.entrySet().equals(actual.entrySet());
+    protected boolean matchesSafely(final Map<KEY, VALUE> actual) {
+
+        missingInExpected = Sets.difference(expected.entrySet(), actual.entrySet());
+        obsoleteInActual = Sets.difference(actual.entrySet(), expected.entrySet());
+
+        if (missingInExpected.isEmpty() && obsoleteInActual.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public void describeTo(final Description description) {
+        description.appendValue(expected);
+    }
 
+
+    @Override
+    protected void describeMismatchSafely(final Map<KEY, VALUE> item, final Description description) {
+
+        super.describeMismatchSafely(item, description);
+
+        if (!missingInExpected.isEmpty()) {
+            newLine(description);
+            description.appendText("Elements missing from expected: ");
+            description.appendValue(missingInExpected);
+        }
+
+        if (!obsoleteInActual.isEmpty()) {
+            newLine(description);
+            description.appendText("Elements obsolete in actual: ");
+            description.appendValue(obsoleteInActual);
+        }
+    }
+
+    private void newLine(final Description description) {
+        description.appendText(String.format("%n"));
     }
 }
--- src/test/java/mapreduceinjava8/wordcount/HasEqualContentsTest.java
+++ src/test/java/mapreduceinjava8/wordcount/HasEqualContentsTest.java
@@ -3,12 +3,14 @@ package mapreduceinjava8.wordcount;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import mapreduceinjava8.utils.HasEqualContents;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Map;
 
 import static mapreduceinjava8.utils.HasEqualContents.hasEqualContents;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class HasEqualContentsTest {
@@ -17,7 +19,7 @@ public class HasEqualContentsTest {
     public void different_implementations_should_be_equal() {
 
         // given
-        final Map<String, Integer> one = new ImmutableMap.Builder()
+        final Map<String, Integer> one = ImmutableMap.<String, Integer>builder()
                 .put("one", 1).build();
 
         final Map<String, Integer> two = Maps.newHashMap();
@@ -27,8 +29,17 @@ public class HasEqualContentsTest {
         assertThat(one, hasEqualContents(two));
     }
 
-    @Test
+    @Test(expected = AssertionError.class)
     public void keys_must_be_equal() {
 
+        // given
+        final Map<String, Integer> actual = ImmutableMap.<String, Integer>builder()
+                .put("one", 1).build();
+
+        final Map<String, Integer> expected = Maps.newHashMap();
+        expected.put("two", 1);
+
+        // then
+        assertThat(actual, hasEqualContents(expected));
     }
 }
