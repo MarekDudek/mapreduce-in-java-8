a9dc01f9d235e2350230af1fc5c64cbecc0b7e0a
Marek Dudek <marek.dudek@gmail.com>
2015-03-22 20:35:46 +0100

    Max temperature computed for single file (year)

--- src/test/java/mapreduceinjava8/maxtemperature/MaxTemperatureTest.java
+++ src/test/java/mapreduceinjava8/maxtemperature/MaxTemperatureTest.java
@@ -1,24 +1,31 @@
 package mapreduceinjava8.maxtemperature;
 
-import org.hamcrest.Matchers;
-import org.junit.Assert;
+import mapreducedata.maxtemperature.WeatherDataImporter;
+import mapreducedata.maxtemperature.WeatherDataRecord;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxTemperatureTest {
 
     @Test
-    public void importing_data() {
+    public void max_temperature_from_single_year_should_be_correctly_computed() {
 
-        // when
+        // given
         final List<WeatherDataRecord> records = new WeatherDataImporter().importData("max-temperature/input/1901");
 
-        // then
-        assertThat(records, hasSize(6565));
-    }
+        // when
+        final Integer maxTemperature = records.stream()
+                .filter(record -> record.getAirTemperature() != 9999)
+                .filter(record -> record.getQuality().matches("[01459]"))
+                .map(WeatherDataRecord::getAirTemperature)
+                .reduce(Math::max).get();
 
+        // then
+        assertThat(maxTemperature, is(equalTo(317)));
+    }
 }
